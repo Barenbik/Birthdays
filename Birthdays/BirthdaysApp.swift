0cea230a1086342e6cfb7121d3c77b81e39785e4
Tony Sharples <tonysharples22@me.com>
2024-05-21 13:42:33 +0100

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Tony Sharples on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
